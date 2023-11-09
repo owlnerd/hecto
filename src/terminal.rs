9d51685ac93ced9ed91b542bd7a45886e5b314f0
Ivan 'Owlnerd' Djurkov <iquislav@gmail.com>
2023-11-09 09:31:59 +0100

    C4 - Implement document viewing functionality

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,6 +1,7 @@
 use crate::Position;
 use std::io::{self, stdout, Write};
 use termion::{
+    color,
     event::Key,
     input::TermRead,
     raw::{IntoRawMode, RawTerminal},
@@ -20,7 +21,10 @@ impl Terminal {
     pub fn default() -> Result<Self, std::io::Error> {
         let (width, height) = termion::terminal_size()?;
         Ok(Self {
-            size: Size { width, height },
+            size: Size {
+                width,
+                height: height.saturating_sub(2),
+            },
             _stdout: stdout().into_raw_mode()?,
         })
     }
@@ -65,5 +69,21 @@ impl Terminal {
 
     pub fn clear_current_line() {
         print!("{}", termion::clear::CurrentLine);
+    }
+
+    pub fn set_bg_color(color: color::Rgb) {
+        print!("{}", color::Bg(color));
+    }
+
+    pub fn reset_bg_color() {
+        print!("{}", color::Bg(color::Reset));
+    }
+
+    pub fn set_fg_color(color: color::Rgb) {
+        print!("{}", color::Fg(color));
+    }
+
+    pub fn reset_fg_color() {
+        print!("{}", color::Fg(color::Reset));
     }
 }
